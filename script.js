e9570e7ebe433f5226e24622c9e5949b2b896aa4
Tito-Rollis <ouvalres@gmail.com>
2021-02-21 14:35:51 +0700

    adding javascript and responsive

---
+++ script.js
@@ -0,0 +1,11 @@
+class Fighter{
+    constructor(chosen){
+        this.chosen = chosen;
+    }
+};
+
+// let p_paper = document.getElementById('player-paper');
+// let p_rock = document.getElementById('player-rock');
+// let p_scissors = document.getElementById('player-scissors');
+let value = document.addEventListener('click',n=> console.log(n,player1))
+let player1 = new Fighter(value);
